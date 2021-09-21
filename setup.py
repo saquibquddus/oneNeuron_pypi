fd1e0cbb0459248fc08b4205a80d3bfd56d7ed38
saquib quddus <sakibquddus@gmail.com>
2021-09-21 09:10:04 +0530

    workflow added version 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "saquibquddus"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.2",
+    version="0.0.3",
     author=USER_NAME,
     author_email="sakibquddus@gmail.com",
     description="It's a implementation of Perceptron",
